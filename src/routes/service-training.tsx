499de563ab413dcf366f8a8278d6f2fd55cbbc8f
Sylvia Awoudu <sionijmeijer@gmail.com>
2026-04-23 14:21:36 +0100

    Update route definition to use 'as any' type assertion

--- src/routes/service-training.tsx
+++ src/routes/service-training.tsx
@@ -14,7 +14,7 @@ import {
   Package,
 } from 'lucide-react'
 
-export const Route = createFileRoute('/service-training')({
+export const Route = createFileRoute('/service-training' as any)({
   component: TrainingPage,
 })
 
